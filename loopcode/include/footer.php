1f84931c6ec739d76dd4a3f732fdf4e3f7af9751
Paulina Lubasińska <lubasinska.paulina@gmail.com>
2023-03-30 13:33:39 +0200

    fix

--- loopcode/include/footer.php
+++ loopcode/include/footer.php
@@ -22,9 +22,9 @@
     <div id="cookies" class="fixed-bottom-cookies d-none">
         <p class="mb-3">Serwis wykorzystuje pliki cookies m.in. w celu poprawienia jesj dostępności, personalizacji oraz aby zbierać dane, dotyczące ruchu na stronie. Każdy może sam decydować o tym czy dopuszcza pliki cookies, ustawiając odpowiednio swoją przeglądarkę.</p>
 
-        <p class="mb-3">Więcej informacji znajdziesz w <a href="polityka-prywatności" class="link">polityce prywatności.</a></p>
-        <button class="main-button js-cookies mr-3">Akceptuję</button>
-        <button class="main-button js-cookies-nope" style="background-color: #777">Odmawiam</button>
+        <p class="mb-3">Więcej informacji znajdziesz w <a href="polityka-prywatności" class="link-policy">polityce prywatności.</a></p>
+        <button class="main-button js-cookies mr-3" style="font-size: 13px">Akceptuję</button>
+        <button class="main-button js-cookies-nope mt-2" style="background-color: #777 font-size: 13px;">Odmawiam</button>
     </div>
     <!-- jQuery -->
     <script src="assets/js/jquery-2.1.0.min.js"></script>
